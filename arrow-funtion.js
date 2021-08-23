0196eda33b74b33a4ad8df46208fd48cda116f73
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-08-23 23:30:43 +0600

    es6 addad

---
+++ arrow-funtion.js
@@ -0,0 +1,27 @@
+// const myObject= {
+//     name:'name',
+//     age:38
+//     city:'cumilla'
+// }
+// getElemensByTa
+
+
+add = (a, b) => a + b;
+
+const result = add(6, 7);
+
+console.log(result);
+
+const sum = (num1, num2) => num1 + num2;
+const value = sum(675, 7657);
+console.log(value);
+
+const value1 = (num1, num2, num3) => {
+    const sum = num2 + num3;
+    const dife = num1 - num3;
+    const mul = sum * dife;
+    return mul;
+}
+
+const result1 = value1(20, 15, 10);
+console.log(result1);
---
+++ index.js
@@ -0,0 +1,22 @@
+//javasript //
+var num = 5;
+function sayName() {
+    var name  =" farhad"
+    console.log(name);
+}
+sayName()
+let counter = 10;
+conunter = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+// ES6 ///
